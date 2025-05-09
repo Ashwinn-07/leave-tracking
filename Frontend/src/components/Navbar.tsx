4271976c04bd6c3fb7a7cced9281788c7e0eefcc
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-05-09 14:12:53 +0530

    feat: add login page

--- Frontend/src/components/Navbar.tsx
+++ Frontend/src/components/Navbar.tsx
@@ -1,6 +1,8 @@
 import { Clock } from "lucide-react";
+import { useNavigate } from "react-router-dom";
 
 const Navbar = () => {
+  const navigate = useNavigate();
   return (
     <nav className="bg-white shadow-sm py-4 px-6 fixed w-full top-0 z-10">
       <div className="max-w-7xl mx-auto flex justify-between items-center">
@@ -9,7 +11,10 @@ const Navbar = () => {
           <span className="text-xl font-semibold text-gray-800">TimeTrack</span>
         </div>
 
-        <button className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors duration-200 cursor-pointer">
+        <button
+          className="bg-blue-600 hover:bg-blue-700 text-white px-4 py-2 rounded-md text-sm font-medium transition-colors duration-200 cursor-pointer"
+          onClick={() => navigate("/login")}
+        >
           Login
         </button>
       </div>
--- Frontend/src/routes/AppRoutes.tsx
+++ Frontend/src/routes/AppRoutes.tsx
@@ -1,10 +1,12 @@
 import { Routes, Route } from "react-router-dom";
 import Landing from "../pages/Landing";
+import Login from "../pages/auth/Login";
 
 export const AppRoutes = () => {
   return (
     <Routes>
       <Route path="/" element={<Landing />} />
+      <Route path="/login" element={<Login />} />
     </Routes>
   );
 };
