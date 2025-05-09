1252331daab0f0a8d8d59193179dfe482c436203
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-05-09 14:30:56 +0530

    feat: add toast

--- Frontend/src/App.tsx
+++ Frontend/src/App.tsx
@@ -1,9 +1,21 @@
 import { BrowserRouter } from "react-router-dom";
 import { AppRoutes } from "./routes/AppRoutes";
+import { Toaster } from "react-hot-toast";
 const App = () => {
   return (
     <BrowserRouter>
       <AppRoutes />
+      <Toaster
+        position="top-center"
+        toastOptions={{
+          style: {
+            background: "#1f2937",
+            color: "#fff",
+            border: "1px solid #4f46e5",
+            boxShadow: "0 0 10px rgba(79, 70, 229, 0.3)",
+          },
+        }}
+      />
     </BrowserRouter>
   );
 };
