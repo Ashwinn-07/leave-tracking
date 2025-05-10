e388e652bc3e5603e06408219745109c472f7961
Ashwinn-07 <ashwinkrishna785@gmail.com>
2025-05-10 12:21:40 +0530

    feat: setup LeaveType model

---
+++ Backend/src/models/leaveType.model.ts
@@ -0,0 +1,26 @@
+import mongoose, { Schema, Document } from "mongoose";
+
+export interface ILeaveType extends Document {
+  _id: string;
+  name: string;
+  description?: string;
+  maxDays: number;
+  accrualRate: number;
+  halfDayAllowed: boolean;
+  createdAt?: Date;
+  updatedAt?: Date;
+}
+
+const leaveTypeSchema = new Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    description: { type: String },
+    maxDays: { type: Number, required: true },
+    accrualRate: { type: Number, required: true },
+    halfDayAllowed: { type: Boolean, default: false },
+  },
+  { timestamps: true }
+);
+
+const leaveType = mongoose.model<ILeaveType>("LeaveType", leaveTypeSchema);
+export default leaveType;
